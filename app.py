ccf9b05a9355f7f5d7fd2e119f13235110df5563
Mark Chang <ak0922747401@gmail.com>
2018-04-19 00:00:48 +0800

    add email messgae

--- app.py
+++ app.py
@@ -22,16 +22,29 @@ config.read('config.ini')
 line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
 user_id = config['line_bot']['User_Id']
-chat_bot = ChatBotService()
+# chat_bot = ChatBotService()
 lock = threading.Lock()
 
 @app.route("/notify", methods=['GET'])
 def notify():
-    app.logger.info("notify body")
+    email_message = '小區名稱:延平小區 商校街 100mm\n' + \
+        '事件名稱:壓力低於歷史量\n' + \
+        '事件日期2018-02-06~2018-02-08\n' + \
+        '事件日期2018-02-07~2018-02-08\n' + \
+        '事件日期2018-02-07~2018-02-09\n' + \
+        '事件日期2018-02-08~2018-02-09\n' + \
+        '事件日期2018-02-08~2018-02-10\n' + \
+        '事件日期2018-02-09~2018-02-10\n' + \
+        '事件日期2018-02-09~2018-02-11\n' + \
+        '事件持續天數:2\n' + \
+        '允許雜質比(%):5\n' + \
+        '誤差比率(%):20\n' + \
+        '事件數目:72\n' + \
+        '預估漏水量:-36.11\n'
     try:
         line_bot_api.push_message(
             user_id,
-            TextSendMessage(text="通知！")
+            TextSendMessage(text=email_message)
         )
     except LineBotApiError:
         abort(400)
@@ -89,8 +102,9 @@ def handle_message(event):
             buttons_template_message
         )
     else:
-        with lock:
-            response = chat_bot.ask_question(event.message.text)
+        # with lock:
+        #     response = chat_bot.ask_question(event.message.text)
+        response = '寶寶聽不懂，所以寶寶不說QQ'
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=response)
