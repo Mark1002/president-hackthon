163552bc62bcb5abaa7c1d2a500b6b32e4247d1c
Mark Chang <ak0922747401@gmail.com>
2018-05-05 18:43:32 +0800

    add map event service

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import configparser
 import threading
+import logging
+import os
+import urllib
+from logging.handlers import RotatingFileHandler
 from chatbot_service import ChatBotService
 from wit_ai import WitAIService
 from flask import Flask, request, abort, jsonify
+from flask import send_file
 from linebot.models import TemplateSendMessage
 from linebot.models import ButtonsTemplate
 from linebot.models import MessageTemplateAction
@@ -13,7 +18,7 @@ from linebot.exceptions import (
     InvalidSignatureError, LineBotApiError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -26,6 +31,8 @@ user_id = config['line_bot']['User_Id']
 # chat_bot = ChatBotService()
 wit_ai = WitAIService()
 lock = threading.Lock()
+region = ''
+location = ''
 
 @app.route("/notify", methods=['POST'])
 def notify():
@@ -71,14 +78,15 @@ def callback():
 
     return 'OK'
 
+@app.route('/image/<region>/<location>/<service_type>')
+def get_image(region, location, service_type):
+    app.logger.info(location)
+    file_path = 'data/' + region + '/' + location + '/' + service_type + '.png'
+    return send_file(file_path, mimetype='image/png')
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == '查詢管線狀況':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='查詢管線狀況功能實作中！')
-        )
-    elif event.message.text == '選單':
+    if event.message.text == '選單':
         buttons_template_message = TemplateSendMessage(
             alt_text='服務功能 template',
             template=ButtonsTemplate(
@@ -87,12 +95,7 @@ def handle_message(event):
                 text='請選擇',
                 actions=[
                     MessageTemplateAction(
-                        label='查詢漏水區域',
-                        text='查詢漏水區域'
-                    ),
-                    MessageTemplateAction(
-                        label='查詢管線狀況',
-                        text='查詢管線狀況'
+                        label='漏水小區狀態查詢',
                     )
                 ]
             )
@@ -101,10 +104,48 @@ def handle_message(event):
             event.reply_token,
             buttons_template_message
         )
+    elif '澎湖' in event.message.text:
+        global region
+        region = '澎湖'
+        location_list = os.listdir('data/澎湖')
+        response = '您已選擇' + '[' + region + ']小區，' + \
+        '可選擇以下共' + str(len(location_list)) + '個地點來查看漏水小區域資訊\n' + \
+        '[' + ','.join(location_list) + ']'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response)
+        )
+    elif event.message.text in os.listdir('data/' + region):
+        global location
+        location = event.message.text
+        response = '您已選擇' + '[' + location + ']小區地點，可輸入[event]查看漏水事件圖' + \
+        '[map]查看管線分佈位置地圖'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response)
+        )
+
+    elif event.message.text == 'map' or event.message.text == 'event':
+        region_encoded = urllib.parse.quote(region)
+        location_encoded = urllib.parse.quote(location)
+        service_type = event.message.text
+        image_url = config['server']['url'] + '/image/' + region_encoded + '/' + location_encoded + '/' + service_type
+        app.logger.info("image url: {}".format(image_url))
+        line_bot_api.reply_message(
+            event.reply_token,
+            ImageSendMessage(
+                original_content_url=image_url,
+                preview_image_url=image_url
+            )
+        )
     else:
         entity_list = wit_ai.retrive_message_entity(event.message.text)
         if 'region' in entity_list:
-            response = '好的，請說要查詢的漏水小區'
+            region_list = os.listdir('data')
+            region_str = '[' + ','.join(region_list) + ']'
+            response = '總計有' + region_str + '共' + str(len(region_list)) + '個小區\n' + \
+                       '請說要查詢的漏水小區名稱'
+            app.logger.info('info')
         else:
             response = '寶寶聽不懂，所以寶寶不說QQ'
         line_bot_api.reply_message(
@@ -112,4 +153,7 @@ def handle_message(event):
             TextSendMessage(text=response)
         )
 if __name__ == "__main__":
+    log_handler = RotatingFileHandler('debug.log', maxBytes=10000, backupCount=1)
+    log_handler.setLevel(logging.INFO)
+    app.logger.addHandler(log_handler)
     app.run(debug=True)
